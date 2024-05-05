bffe7cb6913fa41fdbb36bff3887e08dc17ce459
Jatin Tiwari <113410552+jatintiwari0@users.noreply.github.com>
2024-05-05 10:03:08 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,11 +1,11 @@
 <?php
 
 date_default_timezone_set('Asia/Kolkata');
-define('DB_SERVER', '135.181.161.167'); //localhost
-define('DB_USERNAME', 'byiribpb_ott'); // db username
-define('DB_PASSWORD', 'byiribpb_ott'); // db password
-define('DB_DATABASE', 'byiribpb_ott'); // db name
-define('PAYTM_UPI_ID', 'paytmqr1rudv8w05q@paytm');//your paytm upi id
+define('DB_SERVER', '#'); //localhost
+define('DB_USERNAME', '#'); // db username
+define('DB_PASSWORD', '#'); // db password
+define('DB_DATABASE', '#'); // db name
+define('PAYTM_UPI_ID', '#');//your paytm upi id
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if ($conn->connect_error) {
